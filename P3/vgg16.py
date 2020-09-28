d3acc486c0d4e00b7ee57b855940ee2612de3513
Hong Pan <61482421+Hong-Swinburne@users.noreply.github.com>
2020-09-28 10:45:26 +1000

    Add files via upload

---
+++ P3/vgg16.py
@@ -0,0 +1,161 @@
+# use pretrained weights on Imagenet, add include_top options in function parameters
+import tensorflow as tf
+from tensorflow.python.keras.utils import data_utils
+
+WEIGHTS_PATH = ('https://storage.googleapis.com/tensorflow/keras-applications/'
+                'vgg16/vgg16_weights_tf_dim_ordering_tf_kernels.h5')
+WEIGHTS_PATH_NO_TOP = ('https://storage.googleapis.com/tensorflow/'
+                       'keras-applications/vgg16/'
+                       'vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5')
+
+def vgg16(image_height, image_width, channels, NUM_CLASSES, include_top=True, weights=None, pooling=None):
+    model = tf.keras.Sequential()
+    # 1
+    model.add(tf.keras.layers.Conv2D(filters=64,
+                                     kernel_size=(3, 3),
+                                     strides=1,
+                                     padding='same',
+                                     activation=tf.keras.activations.relu,
+                                     name='block1_conv1',
+                                     input_shape=(image_height, image_width, channels)))
+    model.add(tf.keras.layers.Conv2D(filters=64,
+                                     kernel_size=(3, 3),
+                                     strides=1,
+                                     padding='same',
+                                     name='block1_conv2',
+                                     activation=tf.keras.activations.relu))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2),
+                                        strides=2,
+                                        name='block1_pool',
+                                        padding='same'))
+
+    # 2
+    model.add(tf.keras.layers.Conv2D(filters=128,
+                                     kernel_size=(3, 3),
+                                     strides=1,
+                                     padding='same',
+                                     name='block2_conv1',
+                                     activation=tf.keras.activations.relu))
+    model.add(tf.keras.layers.Conv2D(filters=128,
+                                     kernel_size=(3, 3),
+                                     strides=1,
+                                     padding='same',
+                                     name='block2_conv2',
+                                     activation=tf.keras.activations.relu))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2),
+                                        strides=2,
+                                        name='block2_pool',
+                                        padding='same'))
+
+    # 3
+    model.add(tf.keras.layers.Conv2D(filters=256,
+                                     kernel_size=(3, 3),
+                                     strides=1,
+                                     padding='same',
+                                     name='block3_conv1',
+                                     activation=tf.keras.activations.relu))
+    model.add(tf.keras.layers.Conv2D(filters=256,
+                                     kernel_size=(3, 3),
+                                     strides=1,
+                                     padding='same',
+                                     name='block3_conv2',
+                                     activation=tf.keras.activations.relu))
+    model.add(tf.keras.layers.Conv2D(filters=256,
+                                     kernel_size=(3, 3),
+                                     strides=1,
+                                     padding='same',
+                                     name='block3_conv3',
+                                     activation=tf.keras.activations.relu))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2),
+                                        strides=2,
+                                        name='block3_pool',
+                                        padding='same'))
+
+    # 4
+    model.add(tf.keras.layers.Conv2D(filters=512,
+                                    kernel_size=(3, 3),
+                                    strides=1,
+                                    padding='same',
+                                    name='block4_conv1',
+                                    activation=tf.keras.activations.relu))
+    model.add(tf.keras.layers.Conv2D(filters=512,
+                                    kernel_size=(3, 3),
+                                    strides=1,
+                                    padding='same',
+                                    name='block4_conv2',
+                                    activation=tf.keras.activations.relu))
+    model.add(tf.keras.layers.Conv2D(filters=512,
+                                    kernel_size=(3, 3),
+                                    strides=1,
+                                    padding='same',
+                                    name='block4_conv3',
+                                    activation=tf.keras.activations.relu))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2),
+                                        strides=2,
+                                        name='block4_pool',
+                                        padding='same'))
+
+    # 5
+    model.add(tf.keras.layers.Conv2D(filters=512,
+                                    kernel_size=(3, 3),
+                                    strides=1,
+                                    padding='same',
+                                    name='block5_conv1',
+                                    activation=tf.keras.activations.relu))
+    model.add(tf.keras.layers.Conv2D(filters=512,
+                                    kernel_size=(3, 3),
+                                    strides=1,
+                                    padding='same',
+                                    name='block5_conv2',
+                                    activation=tf.keras.activations.relu))
+    model.add(tf.keras.layers.Conv2D(filters=512,
+                                    kernel_size=(3, 3),
+                                    strides=1,
+                                    padding='same',
+                                    name='block5_conv3',
+                                    activation=tf.keras.activations.relu))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2),
+                                        strides=2,
+                                        name='block5_pool',
+                                        padding='same'))
+
+    if include_top:
+        # Classification block
+        model.add(tf.keras.layers.Flatten())
+        model.add(tf.keras.layers.Dense(units=4096,
+                                        activation=tf.keras.activations.relu))
+        model.add(tf.keras.layers.Dense(units=4096,
+                                        activation=tf.keras.activations.relu))
+        model.add(tf.keras.layers.Dropout(rate=0.5))
+
+        model.add(tf.keras.layers.Dense(units=NUM_CLASSES,
+                                    activation=tf.keras.activations.softmax))
+    else:
+        if pooling == 'avg':
+            model.add(tf.keras.layers.GlobalAveragePooling2D())
+        elif pooling == 'max':
+            model.add(tf.keras.layers.GlobalMaxPooling2D())
+
+    if weights == 'imagenet':
+        if include_top:
+            weights_path = data_utils.get_file(
+            'vgg16_weights_tf_dim_ordering_tf_kernels.h5',
+            WEIGHTS_PATH,
+            cache_subdir='models',
+            file_hash='64373286793e3c8b2b4e3219cbf3544b')
+        else:
+            weights_path = data_utils.get_file(
+                'vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                WEIGHTS_PATH_NO_TOP,
+                cache_subdir='models',
+                file_hash='6d6bbae143d832006294945121d1f1fc')
+        model.load_weights(weights_path)
+    elif weights is not None:
+        model.load_weights(weights)
+
+    return model
